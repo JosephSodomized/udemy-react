d01a3e45bdcb09ba1b82d50c0252493abb8faa13
--global <karo.rutkowska@gmail.com>
2019-04-25 15:26:14 +0200

    component lifecycle methods added

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Persons from './Person/Person';
 
-const persons = (props) => props.persons.map( (person, index) => {
-        return <Persons 
-        click={() => props.clicked(index)}
-        name={person.name} 
-        age={person.age}   
-        key={person.id}           
-        changed={(event) => props.changed(event, person.id)} />
-        
-      } );
+const persons = props => {
+    
+    console.log('[Persons.js] rendering ...');
+    return props.persons.map( (person, index) => {
+            return ( 
+                <Persons 
+                    click={() => props.clicked(index)}
+                    name={person.name} 
+                    age={person.age}   
+                    key={person.id}           
+                    changed={(event) => props.changed(event, person.id)}
+                />
+            );
+            
+        });
+};
 
 
 export default persons;
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,13 @@ import Cockpit from '../components/Cockpit/Cockpit';
 
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+
+  }
+
   state = {
     persons: [
       { id: 'sadfg', name : 'Karo', age: 21},
@@ -15,11 +22,21 @@ class App extends Component {
     showPersons: false
   }
 
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
   deletePersonHandler = (personIndex) => {
     //const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
     this.setState({persons: persons});
+  }
+
+  componentDidMount() {
+    console.log('[App.js] component did mount');
   }
 
   nameChangedHandler = (event, id) => {
@@ -47,6 +64,7 @@ class App extends Component {
   
   render() {
 
+    console.log('[App,js] render');
     let persons = null;
 
     if (this.state.showPersons) {
@@ -62,6 +80,7 @@ class App extends Component {
     return (
       <div className={classes.App}>
         <Cockpit 
+        title={this.props.appTitle}
         showPersons={this.state.showPersons}
         persons={this.state.persons}
         clicked={this.togglePersonsHandler} />
