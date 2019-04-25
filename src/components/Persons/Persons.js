9bdaa5ba528b32cd08aab8cff0b9d7de4d51d3c9
--global <karo.rutkowska@gmail.com>
2019-04-25 13:18:31 +0200

    Person structure improved

---
+++ src/components/Persons/Persons.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Persons from './Person/Person';
+
+const persons = (props) => props.persons.map( (person, index) => {
+        return <Persons 
+        click={() => props.clicked(index)}
+        name={person.name} 
+        age={person.age}   
+        key={person.id}           
+        changed={(event) => props.changed(event, person.id)} />
+        
+      } );
+
+
+export default persons;
